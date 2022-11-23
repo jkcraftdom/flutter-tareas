a56529bfedb67905fa9d31897d7295cfcbf23963
Jheremy <67889063+jkcraftdom@users.noreply.github.com>
2022-11-23 11:51:28 -0500

    sesion 12

--- safeperfil/safeperfil/lib/screen/login_screen.dart
+++ safeperfil/safeperfil/lib/screen/login_screen.dart
@@ -4,8 +4,21 @@ import 'package:provider/provider.dart';
 import 'package:safeperfil/providers/login_provider.dart';
 import 'package:safeperfil/routes/route.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  bool _isPassword = true;
+
+  void _viewPassword() {
+    setState(() {
+      _isPassword = !_isPassword;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,104 +26,119 @@ class LoginScreen extends StatelessWidget {
 
     return Scaffold(
       backgroundColor: Colors.black,
-      body: Center(
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              SizedBox(
-                width: 300,
-                child: Lottie.network(
-                    'https://assets4.lottiefiles.com/packages/lf20_mjlh3hcy.json'),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 10),
-                child: Form(
-                    key: loginProvider.formKey,
-                    child: Column(
-                      children: [
-                        TextFormField(
-                          style: const TextStyle(color: Colors.black),
-                          autocorrect: false,
-                          keyboardType: TextInputType.emailAddress,
-                          decoration: _buildDecoration(
-                              hintText: 'dev@flutter.com',
-                              prefixIcon: const Icon(
-                                Icons.email_outlined,
-                                color: Colors.grey,
-                              )),
-                          onChanged: (value) => loginProvider.email = value,
-                          validator: (value) {
-                            String caracteres =
-                                r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
-                            RegExp regExp = RegExp(caracteres);
+      body: GestureDetector(
+        onTap: () {
+          final FocusScopeNode focus = FocusScope.of(context);
+          if (!focus.hasPrimaryFocus && focus.hasFocus) {
+            FocusManager.instance.primaryFocus!.unfocus();
+          }
+        },
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  width: 300,
+                  child: Lottie.network(
+                      'https://assets4.lottiefiles.com/packages/lf20_mjlh3hcy.json'),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 10),
+                  child: Form(
+                      key: loginProvider.formKey,
+                      child: Column(
+                        children: [
+                          TextFormField(
+                            style: const TextStyle(color: Colors.black),
+                            autocorrect: false,
+                            keyboardType: TextInputType.emailAddress,
+                            decoration: _buildDecoration(
+                                hintText: 'dev@flutter.com',
+                                prefixIcon: const Icon(
+                                  Icons.email_outlined,
+                                  color: Colors.grey,
+                                )),
+                            onChanged: (value) => loginProvider.email = value,
+                            validator: (value) {
+                              String caracteres =
+                                  r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+                              RegExp regExp = RegExp(caracteres);
 
-                            return regExp.hasMatch(value ?? '')
-                                ? null
-                                : 'No es un correo  valido';
-                          },
-                        ),
-                        const SizedBox(
-                          height: 10,
-                        ),
-                        TextFormField(
-                          style: const TextStyle(color: Colors.black),
-                          autocorrect: false,
-                          keyboardType: TextInputType.text,
-                          obscureText: true,
-                          decoration: _buildDecoration(
-                              hintText: '*****',
-                              prefixIcon: const Icon(
-                                Icons.password,
-                                color: Colors.grey,
-                              )),
-                          onChanged: (value) => loginProvider.password = value,
-                          validator: (value) {
-                            return (value != null && value.length >= 8)
-                                ? null
-                                : 'Debe tener minimo 8 caracteres';
-                          },
-                        ),
-                        const SizedBox(
-                          height: 20,
-                        ),
-                        SizedBox(
-                          height: 50,
-                          width: 250,
-                          child: MaterialButton(
-                            onPressed: loginProvider.isLoading
-                                ? null
-                                : () async {
-                                    FocusScope.of(context).unfocus();
-                                    if (!loginProvider.isValidForm()) return;
-
-                                    loginProvider.isLoading = true;
-
-                                    await Future.delayed(
-                                        const Duration(seconds: 2));
-
-                                    loginProvider.isLoading = false;
-
-                                    // ignore: use_build_context_synchronously
-                                    Navigator.pushReplacementNamed(
-                                        context, MyRoutes.rHome);
-                                  },
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(15)),
-                            disabledColor: Colors.green,
-                            elevation: 1,
-                            color: Colors.green,
-                            child: (loginProvider.isLoading)
-                                ? const CircularProgressIndicator(
-                                    color: Colors.white,
-                                  )
-                                : const Text('INGRESAR'),
+                              return regExp.hasMatch(value ?? '')
+                                  ? null
+                                  : 'No es un correo  valido';
+                            },
                           ),
-                        )
-                      ],
-                    )),
-              )
-            ],
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          TextFormField(
+                            style: const TextStyle(color: Colors.black),
+                            autocorrect: false,
+                            keyboardType: TextInputType.text,
+                            obscureText: _isPassword,
+                            decoration: _buildDecoration(
+                                suffixIcon: InkWell(
+                                  onTap: _viewPassword,
+                                  child: Icon(_isPassword
+                                      ? Icons.visibility
+                                      : Icons.visibility_off),
+                                ),
+                                hintText: '*****',
+                                prefixIcon: const Icon(
+                                  Icons.password,
+                                  color: Colors.grey,
+                                )),
+                            onChanged: (value) =>
+                                loginProvider.password = value,
+                            validator: (value) {
+                              return (value != null && value.length >= 8)
+                                  ? null
+                                  : 'Debe tener minimo 8 caracteres';
+                            },
+                          ),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          SizedBox(
+                            height: 50,
+                            width: 250,
+                            child: MaterialButton(
+                              onPressed: loginProvider.isLoading
+                                  ? null
+                                  : () async {
+                                      FocusScope.of(context).unfocus();
+                                      if (!loginProvider.isValidForm()) return;
+
+                                      loginProvider.isLoading = true;
+
+                                      await Future.delayed(
+                                          const Duration(seconds: 2));
+
+                                      loginProvider.isLoading = false;
+
+                                      // ignore: use_build_context_synchronously
+                                      Navigator.pushReplacementNamed(
+                                          context, MyRoutes.rHome);
+                                    },
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(15)),
+                              disabledColor: Colors.green,
+                              elevation: 1,
+                              color: Colors.green,
+                              child: (loginProvider.isLoading)
+                                  ? const CircularProgressIndicator(
+                                      color: Colors.white,
+                                    )
+                                  : const Text('INGRESAR'),
+                            ),
+                          )
+                        ],
+                      )),
+                )
+              ],
+            ),
           ),
         ),
       ),
@@ -120,6 +148,7 @@ class LoginScreen extends StatelessWidget {
   InputDecoration _buildDecoration({
     final String? hintText,
     final Widget? prefixIcon,
+    final Widget? suffixIcon,
   }) {
     return InputDecoration(
         filled: true,
@@ -127,6 +156,7 @@ class LoginScreen extends StatelessWidget {
         hintText: hintText,
         hintStyle: const TextStyle(color: Colors.grey),
         prefixIcon: prefixIcon,
+        suffixIcon: suffixIcon,
         contentPadding: const EdgeInsets.all(18),
         errorStyle: const TextStyle(fontSize: 18),
         errorBorder: OutlineInputBorder(
